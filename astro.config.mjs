e1f09d25ad81da69021aa338df5b92b4045caf5c
jonathan youngblood <jonathan.youngblood@gmail.com>
2026-06-05 13:54:57 -0500

    dzn polish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      name: 'Geist',
+      cssVariable: '--font-geist-sans',
+      provider: fontProviders.google(),
+      weights: [400, 500, 600],
+      styles: ['normal'],
+    },
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
